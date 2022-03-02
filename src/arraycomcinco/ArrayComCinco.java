26c3d568d2888ffeb46e462c884e0c0310b5abf6
FabinhuSilva <fabiano.fesilva@gmail.com>
2022-03-01 23:17:26 -0300

    array com 5 valores maxima/minima/media

---
+++ src/arraycomcinco/ArrayComCinco.java
@@ -0,0 +1,26 @@
+
+package arraycomcinco;
+
+/**
+ *
+ * @author Fabinhu
+ */
+public class ArrayComCinco {
+
+    
+    public static void main(String[] args) {
+    
+        //criandos variaveis do Tipo ARRAY
+        double[] Notas = new double[5];
+        String[] Alunos = new String[5];
+        
+        double[] Notas = {2,3.5,7.2,8,10};
+        String[] Alunos = {"Pedro","Paulo","Jose","Joao","Daniel"};
+        
+        //testando ARRAYs
+        System.out.println(Notas[]);
+        System.out.println(Alunos[]);
+        
+    }
+    
+}
