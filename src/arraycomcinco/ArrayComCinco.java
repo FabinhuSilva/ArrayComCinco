2fb2424ca4a93b57c8ca472df2f6b261aaffb51f
FabinhuSilva <fabiano.fesilva@gmail.com>
2022-03-02 00:28:04 -0300

    criado ARRAY trabalhando os calculos

--- src/arraycomcinco/ArrayComCinco.java
+++ src/arraycomcinco/ArrayComCinco.java
@@ -9,17 +9,35 @@ public class ArrayComCinco {
 
     
     public static void main(String[] args) {
-    
-        //criandos variaveis do Tipo ARRAY
-        double[] Notas = new double[5];
-        String[] Alunos = new String[5];
         
-        double[] Notas = {2,3.5,7.2,8,10};
-        String[] Alunos = {"Pedro","Paulo","Jose","Joao","Daniel"};
+        
+        //criandos variaveis do Tipo ARRAY
+        double[] notas = {2,3.5,7.2,8,10,7};
+        String[] alunos = {"Pedro","Paulo","Jose","Joao","Daniel","Fabiano"};
         
         //testando ARRAYs
-        System.out.println(Notas[]);
-        System.out.println(Alunos[]);
+        for(int contador = 0;contador < notas.length;contador++ ){
+            System.out.println("Posição "+contador+" - Valor - "+notas[contador]);
+            System.out.println("Posição "+contador+" - Valor - "+alunos[contador]);
+            System.out.println("\n");
+        }
+  
+        //estanciando ARRAY
+        double CarregarNotas = new notas[5];
+    }
+
+    public ArrayComCinco() {
+    }
+    
+    public void ArrayNotaMedia(){
+        for(int contador=0;contador < notas.length;contador++){
+            
+        }
+    }
+    public void ArrayNotaMaxima(){
+        
+    }
+    public void ArrayNotaMinima(){
         
     }
     
